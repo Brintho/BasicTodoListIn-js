9e0a768cc60f14e0fd9a1f12c612835f191b394d
Brinto Chakroborti Sammo <brinthochokrobatisammo@gmail.com>
2025-05-18 21:03:25 +0600

    2nd commit

--- todo.js
+++ todo.js
@@ -1,3 +1,5 @@
+// Brinto code :  
+// 1. Create a list of todos with title and done status
 let todos = [
     { title: 'Todo 1', done: false },
     { title: 'Todo 2', done: false },
@@ -5,10 +7,12 @@ let todos = [
     { title: 'Todo 4', done: true },
 ]
 
+// 2. Create a function that takes a todo and returns true if the todo is done
 let doneTodos = [];
 let onGoingTodos = [];
 
 
+// 3. Create a function that takes a todo and returns true if the todo is not done
 todos.forEach(todo => {
     if (todo.done) {
         doneTodos.push(todo);
@@ -17,15 +21,15 @@ todos.forEach(todo => {
     }
 })
 
-
+// 4. Print the list of todos with their status
 console.log('✅ Done Todos:');
 doneTodos.forEach(todo => {
     console.log(`- ${todo.title}`);
 })
 
+
+// 5. Print the list of todos with their status
 console.log('\n ❌ On Going Todos:');
 onGoingTodos.forEach(todo => {
     console.log(`- ${todo.title}`);
 })
-
-// Output:      
